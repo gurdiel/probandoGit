78f7efc43aea50b1c88ae225102819bb70ccbc48
Rafael Gurdiel <rsgurdiel@hotmail.com>
2020-10-14 01:05:49 +0200

    Subiendo ejercicios con PildorasINfor

---
+++ probando/src/probando/Declaraciones_operadores.java
@@ -0,0 +1,19 @@
+package probando;
+
+public class Declaraciones_operadores {
+
+	public static void main(String[] args) {
+		
+		int raiz = (int) Math.sqrt(9);
+		
+		final double apulgadas = 2.54;
+		double cm=35;
+		
+		double c=cm/apulgadas;
+		
+		System.out.println("Estoy a " + raiz);
+		
+		
+	}
+
+}
